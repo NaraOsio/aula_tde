662f84f4a8be05374d3db3057eb010258c32795d
osiosilvane@gmail.com <osiosilvane@gmail.com>
2024-03-17 14:02:16 -0300

    áula tde

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿//1-Crie uma variável chamada idade e atribua a ela o valor 35 e exiba o seu valor.
+
+int idade = 35;
+Console.WriteLine(idade);
+
+//2-Crie uma variável chamada nome e atribua o valor "Maria" e exiba o seu valor.
+ string nome = "Maria";
+Console.WriteLine(nome);
+
+//3-Crie uma variável chamada altura e atribua a ela o valor 3.45.
+
+double altura = 3.45;
+Console.WriteLine(altura);
+
+//-4Crie uma constante chamada ano e atribua a ela o valor 12 e exiba o seu valor
+const int ano = 12;
+ Console.WriteLine(ano);
+
+ //5-Declare uma variável nota do tipo double como sendo do tipo Nullable type e atribua o
+//valor 7.80 e exiba o seu valor
+
+double? nota = 7.80;
+Console.WriteLine(nota);
+
+//6-Quais as diferenças entre os tipos por valor e os tipos por referência ?
+//Por valor uma cópia do objeto é passada e a mudança nao altera a origem, já por referência é passado o próprio objeto e a alteração reflete na origem
+
+//7-O que é um nullable type e qual a sua utilidade ?
+//7 - nullable type é um tipo que permite receber valores null. Serve para quando se precisa de uma variável que receba null
+
+
+//8-O que é Camel Case ? Dê um exemplo de sua aplicação.
+//8-Camel case é um padrão de nomenclatura onde nomes compostos se juntam formando uma única palavra, com a primeira letra de cada palavra sendo sempre maiúscula, exceto a da primeira palavra, que deve ser minúscula
+
+int notaMedia = 10;
+Console.WriteLine(notaMedia);
+
+//9-O que é Pascal Case ? Dê um exemplo de sua aplicação
+//Pascal case é uma nomenclatura e cada palavra começa com uma letra maiúscula. 
+
+string NomeAluno = "Rafael";
+Console.WriteLine(NomeAluno);
+
+
+
+//10-Declare duas variáveis x e y como sendo do tipo int e atribua a ela os valores 77 e 66 e a
+//seguir imprima o valor da soma de x com y. (Use o operador aritmético + para realizar a
+//operação de soma)
+
+int x = 77;
+int y = 66;
+
+int soma =  x + y;
+
+Console.WriteLine("A soma de x e y é " + soma);
+
+
+//11-Quais os valores padrões dos tipos de dados bool, char, int, double, float, decimal e
+//string.
+// bool false
+// char caractere
+// int 0;
+// double 0;
+// float 0;
+// decimal 0;
+// string ''''
+
+
+
